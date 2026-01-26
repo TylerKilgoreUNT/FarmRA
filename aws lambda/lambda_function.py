63439c740f416e1fd391cf7d1d31b1e5c6c351e0
Brenden-Morgan <brenden.morgan789@gmail.com>
2026-01-25 19:25:07 -0600

    Updated Lambda Code

--- aws lambda/lambda_function.py
+++ aws lambda/lambda_function.py
@@ -1,23 +1,38 @@
 import psycopg2
 import os
+import boto3
+from botocore.exceptions import ClientError
+
 
 def lambda_handler(event, context):
+
     conn = psycopg2.connect(
-        host="ec2-54-163-26-24.compute-1.amazonaws.com",
+        host="172.31.30.160",
         port="5432",
-        dbname="developer",
-        user="",
-        password=""
+        dbname="farmra",
+        user="user_lambda",
+        password="smilingfriends3"
     )
-    cursor = conn.cursor()
+    
+    try: 
+        cursor = conn.cursor()
 
-#    for record in event['Records']:
-#        body = record['body']
-#        # Insert into PostgreSQL
-#        cursor.execute("INSERT INTO test_tables.sqs_test (message_body) VALUES (%s)", (body,))
-#        conn.commit()
+        for record in event['Records']:
+            body = record['body']
+            # Insert into PostgreSQL
+            cursor.execute("INSERT INTO testing_grounds.lambda_test (text_field) VALUES (%s)", (body,))
+        conn.commit()
 
-    cursor.close()
-    conn.close()
+    except (Exception, psycopg2.Error) as error:
+        print("Error while connecting to PostgreSQL", error)
+        if conn:
+            conn.rollback()
+        raise
+
+    finally:
+        if cursor: 
+            cursor.close() 
+        if conn: 
+            conn.close()
+
     return {"status": "success"}
-
