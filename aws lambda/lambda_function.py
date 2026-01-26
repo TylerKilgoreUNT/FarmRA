eb4d0bb888f93abda6f8bdd871b6b7c32953cb88
Brenden-Morgan <brenden.morgan789@gmail.com>
2026-01-26 14:58:40 -0600

    Implemented Secrets Manager into Lambda function for credentials

--- aws lambda/lambda_function.py
+++ aws lambda/lambda_function.py
@@ -1,17 +1,38 @@
 import psycopg2
 import os
+import json
 import boto3
 from botocore.exceptions import ClientError
 
 
 def lambda_handler(event, context):
 
+    secret_name = "lambda_to_db_secret"
+    region_name = "us-east-1"
+
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        raise e
+    print(get_secret_value_response)
+
+    secret = get_secret_value_response['SecretString']
+    creds = json.loads(secret)
+
     conn = psycopg2.connect(
-        host="172.31.30.160",
-        port="5432",
-        dbname="farmra",
-        user="user_lambda",
-        password="smilingfriends3"
+        host=creds["host"],
+        port=creds["port"],
+        dbname=creds["dbname"],
+        user=creds["username"],
+        password=creds["password"]
     )
     
     try: 
