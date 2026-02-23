bbbf0de48aceca743a9adaf111c154de29af1b67
Brenden-Morgan <brenden.morgan789@gmail.com>
2026-02-22 21:45:15 -0600

    Edited deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -10,9 +10,6 @@ echo "Copying files to /var/www/html..."
 sudo rm -rf /var/www/html/farmra.net/*
 sudo cp -r ~/FarmRA/frontend/* /var/www/html/farmra.net/
 
-# Delete unnecessary files
-sudo rm -rf /var/www/html/farmra.net/database /var/www/html/farmra.net/'aws lambda'
-
 # Copies deployment script to home directory" 
 cp ~/FarmRA/deploy/deploy.sh ~/deploy.sh
 
